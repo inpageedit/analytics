e7d8a1940f07711e0e107cc21985560c581a2918
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2020-12-16 05:01:05 +0800

    Update pretty.js

--- api/v4/module/pretty.js
+++ api/v4/module/pretty.js
@@ -1,5 +1,6 @@
 module.exports = (code) => {
   code = code || ''
+  if (typeof code === 'object') code = JSON.stringify(code, null, 2)
   const hljs = require('highlight.js')
   return hljs.highlightAuto(code).value
 }
