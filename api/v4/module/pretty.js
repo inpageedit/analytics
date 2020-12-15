35b0a948421508a9041959fc7f5a170bbcef08b6
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2020-12-16 05:11:21 +0800

    Update pretty.js

--- api/v4/module/pretty.js
+++ api/v4/module/pretty.js
@@ -2,5 +2,20 @@ module.exports = (code) => {
   code = code || ''
   if (typeof code === 'object') code = JSON.stringify(code, null, 2)
   const hljs = require('highlight.js')
-  return hljs.highlightAuto(code).value
+  code = hljs.highlightAuto(code).value
+  var html = `
+<html>
+  <head>
+    <title>Pretty printed result | InPageEdit Analysis API</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/highlight.js@9.12.0/styles/solarized-light.css">
+  </head>
+  <body>
+    <header><h1>Pretty printed result</h1></header>
+    <main>
+      <pre><code>${code}<code></pre>
+    </main>
+  </body>
+</html>
+  `
+  return html
 }
